bb2c6b785a53e90f3f5ff3e67354b68bc4bea0ad
Jader <swilor29@gmail.com>
2023-07-25 18:32:19 -0500

    feat: :sparkles: url local

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -15,22 +15,23 @@ import { Observable } from 'rxjs';
 export class ApiService {
   percentDone!: number;
   uploadSuccess!: boolean;
-  private _url: string = '';
+  private _url: string = 'http://192.168.1.57:8000/';
 
   constructor(private _http: HttpClient) {}
 
   uploadFile(file: NewFile) {
     let headers = new HttpHeaders();
-    headers = headers
-      .set('Content-Type', 'multipart/form-data');
+    headers = headers.set('Content-Type', 'multipart/form-data');
 
     const fd = new FormData();
     fd.append('image_path', file.image_path!);
     fd.append('label', file.label);
     fd.append('url', file.url!);
+
+    return this._http.post(`${this._url}api/uploadfile`, fd);
   }
 
   getAllImages(): Observable<NewFile[]> {
-    return this._http.get<NewFile[]>('http://192.168.1.57:8000/api/getImages');
+    return this._http.get<NewFile[]>(`${this._url}api/getImages`);
   }
 }
